38fc504c8e9b3ea5a94e662138c0a4156bdaf4cb
JQ <2016quezadaj@gmail.com>
2020-10-22 22:54:54 -0700

    Updated npm start script to open firefox developer edition as default

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
 }
 
 export default Navbar;
+
