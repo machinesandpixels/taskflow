965e560b50e72d3bb7d62738fc60f674333b1683
J <2016quezadaj@gmail.com>
2021-06-20 23:58:12 -0700

    Added boilerplate routes for users and auth

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,2 +1,11 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
+
+// @route   POST api/users
+// @desc    Register a user
+// @access   Private
+router.get('/', (req, res) => {
+    res.send('Registered a user');
+});
+
+module.exports = router;
